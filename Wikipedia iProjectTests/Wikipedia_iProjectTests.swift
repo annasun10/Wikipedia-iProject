081b26145db874a5191648127ba4734100291e29
Anna Sun <145409229+annasun10@users.noreply.github.com>
2025-02-22 16:50:09 -0500

    Initial Commit

---
+++ Wikipedia iProjectTests/Wikipedia_iProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  Wikipedia_iProjectTests.swift
+//  Wikipedia iProjectTests
+//
+//  Created by anna sun on 2/22/25.
+//
+
+import Testing
+@testable import Wikipedia_iProject
+
+struct Wikipedia_iProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Wikipedia iProjectUITests/Wikipedia_iProjectUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Wikipedia_iProjectUITestsLaunchTests.swift
+//  Wikipedia iProjectUITests
+//
+//  Created by anna sun on 2/22/25.
+//
+
+import XCTest
+
+final class Wikipedia_iProjectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
